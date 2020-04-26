014e7c6d931dfb2761c368526191c005fa6f6d65
chen <jubi33717f@gmail.com>
2020-04-26 15:28:19 +1000

    init2
    init2

--- src/pages/browse-tasks/browse-tasks-right/task-detail/side-bar/side-bar.js
+++ src/pages/browse-tasks/browse-tasks-right/task-detail/side-bar/side-bar.js
@@ -81,7 +81,9 @@ class SideBar extends React.Component {
               ) ||
               (status === "completed" && userRole === "assignee"
                 &&
-                <div className="green-button">Leave Review</div>
+                <div className="green-button" onClick={() => {
+                  this.pushToGeneratedUrl('leave-review')
+                }}>Leave Review</div>
               ) ||
               (status === "completed"
                 &&
